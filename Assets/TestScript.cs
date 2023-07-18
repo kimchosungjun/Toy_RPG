9a6675cec6fc8fbdedb198e3a97a8e7255488016
Sung Jun Cho <chosung0531@naver.com>
2023-07-18 21:32:28 +0900

    테스트 스크립트 추가

---
+++ Assets/TestScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    TestScript testScript = new TestScript();
+    
+    void Start()
+    {
+        testScript = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
